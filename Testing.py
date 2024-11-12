087b4de31d89c49b4636c0e732335e43b22a3e77
AbdullahFarooq123 <abdullahfarooq20172018@gmail.com>
2024-11-12 15:51:06 +0500

    Init

--- Testing.py
+++ Testing.py
@@ -4,5 +4,8 @@ from googleapiclient.errors import HttpError
 
 from Helpers.GoogleDriveHelper import GoogleDriverHelper
 
-google_drive_helper = GoogleDriverHelper('Login')
+# google_drive_helper = GoogleDriverHelper('Login')
 # files = google_drive_helper.save_prev_drive_files_on_folder()
+
+with open('Unique Data.json' , 'r') as json_file:
+    print(len(list(json.load(json_file).keys())))
